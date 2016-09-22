ddd8b12164f61f3a51712e527d86fd34ca33b87b
moto <mthrok@gmail.com>
2016-09-22 01:44:24 -0700

    Treat life loss as termination in training mode (#35)
    * Add test/train mode for different life count treatment
    * Do not reset game when training mode and not game over

--- src/environment.py
+++ src/environment.py
@@ -28,6 +28,10 @@ class Environment:
     # Returns if game is done
     raise NotImplementedError
 
+  def setMode(self, mode):
+    # Set training/test mode. Not used in Gym environment
+    self.mode = mode
+
 class ALEEnvironment(Environment):
   def __init__(self, rom_file, args):
     from ale_python_interface import ALEInterface
@@ -73,14 +77,28 @@ class ALEEnvironment(Environment):
     self.screen_width = args.screen_width
     self.screen_height = args.screen_height
 
+    self.life_lost = False
+
   def numActions(self):
     return len(self.actions)
 
   def restart(self):
-    self.ale.reset_game()
+    # In test mode, the game is simply initialized. In train mode, if the game
+    # is in terminal state due to a life loss but not yet game over, then only
+    # life loss flag is reset so that the next game starts from the current
+    # state. Otherwise, the game is simply initialized.
+    if (
+        self.mode == 'test' or
+        not self.life_lost or  # `reset` called in a middle of episode
+        self.ale.game_over()  # all lives are lost
+    ):
+      self.ale.reset_game()
+    self.life_lost = False
 
   def act(self, action):
+    lives = self.ale.lives()
     reward = self.ale.act(self.actions[action])
+    self.life_lost = (not lives == self.ale.lives())
     return reward
 
   def getScreen(self):
@@ -89,6 +107,8 @@ class ALEEnvironment(Environment):
     return resized
 
   def isTerminal(self):
+    if self.mode == 'train':
+      return self.ale.game_over() or self.life_lost
     return self.ale.game_over()
 
 class GymEnvironment(Environment):
